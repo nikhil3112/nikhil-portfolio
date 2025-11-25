bdddd056b426223a647b887743facf828a8c1684
nikhil3112 <55823957+nikhil3112@users.noreply.github.com>
2025-11-25 14:12:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // your repo name
+  plugins: [react],
+  base: "/", // root path for user site
 });
